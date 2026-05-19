68e83c454cc3b8519269e74560d4025962602289
ikechukwuefughu <75021390+ikechukwuefughu@users.noreply.github.com>
2026-05-19 16:06:03 +0100

    config.py

--- app/config.py
+++ app/config.py
@@ -8,18 +8,29 @@
 #     conn.execute("PRAGMA foreign_keys = ON;")
 #     return conn
 
-import sqlite3
+# import sqlite3
 
-DATABASE = r"C:\Users\User\Downloads\Yittbox Python Projects\myenv\fiona_pilot_app\creche.db"
+# DATABASE = r"C:\Users\User\Downloads\Yittbox Python Projects\myenv\fiona_pilot_app\creche.db"
 
-def get_db_connection():
-    conn = sqlite3.connect(
-        DATABASE,
-        timeout=5,              # wait up to 5 seconds for locks instead of failing immediately
-        check_same_thread=False # avoids thread errors with Flask’s threaded server
-    )
-    conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_keys = ON;")
-    # Optional but recommended: enable WAL mode (you can also do this once at init)
-    conn.execute("PRAGMA journal_mode=WAL;")
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect(
+#         DATABASE,
+#         timeout=5,              # wait up to 5 seconds for locks instead of failing immediately
+#         check_same_thread=False # avoids thread errors with Flask’s threaded server
+#     )
+#     conn.row_factory = sqlite3.Row
+#     conn.execute("PRAGMA foreign_keys = ON;")
+#     # Optional but recommended: enable WAL mode (you can also do this once at init)
+#     conn.execute("PRAGMA journal_mode=WAL;")
+#     return conn
+
+import os
+
+class Config:
+    # Flask security key
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # Database connection (Azure SQL)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
