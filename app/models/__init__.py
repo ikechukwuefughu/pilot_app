dbbce38655dd54114ac7b69688530c056db47962
ikechukwuefughu <75021390+ikechukwuefughu@users.noreply.github.com>
2026-05-20 22:50:22 +0100

    __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,17 @@ from app import db
 from .household import Household
 from .parent import Parent
 from .child import Child
+from .child_contract import ChildContract
+from .child_medical_info import ChildMedicalInfo
+from .child_emergency_contact import ChildEmergencyContact
+from .child_parent_relationship import ChildParentRelationship
 
-__all__ = ["Household", "Parent"]
+__all__ = [
+    "Household",
+    "Parent",
+    "Child",
+    "ChildContract",
+    "ChildMedicalInfo",
+    "ChildEmergencyContact",
+    "ChildParentRelationship"
+]
