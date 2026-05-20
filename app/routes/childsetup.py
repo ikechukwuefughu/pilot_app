1d05e2a7435d79431343645b2057a621e444cbe3
ikechukwuefughu <75021390+ikechukwuefughu@users.noreply.github.com>
2026-05-20 07:13:04 +0100

    childsetup.py

--- app/routes/childsetup.py
+++ app/routes/childsetup.py
@@ -1,4 +1,4 @@
-# from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify
 # # from app.config import get_db_connection
 # from app import db
 # from datetime import date
