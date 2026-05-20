f87f2d231da62e08e87ad484011a142bea4b8a62
ikechukwuefughu <75021390+ikechukwuefughu@users.noreply.github.com>
2026-05-20 22:52:08 +0100

    child_medical_info.py

--- app/models/child_medical_info.py
+++ app/models/child_medical_info.py
@@ -1,1 +1,15 @@
+class ChildMedicalInfo(db.Model):
+    __tablename__ = "child_medical_info"
+    __table_args__ = {"schema": "dbo"}
 
+    medical_id = db.Column(db.Integer, primary_key=True)
+
+    child_id = db.Column(
+        db.Integer,
+        db.ForeignKey("dbo.child.child_id", ondelete="CASCADE"),
+        unique=True,
+        index=True
+    )
+
+    allergies = db.Column(db.UnicodeText)
+    medical_notes = db.Column(db.UnicodeText)
