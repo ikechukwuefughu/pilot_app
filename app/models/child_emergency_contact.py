ce25047c534f9c7a55a9f7fc9e759644701cf23a
ikechukwuefughu <75021390+ikechukwuefughu@users.noreply.github.com>
2026-05-20 22:52:30 +0100

    child_emergency_contact.py

--- app/models/child_emergency_contact.py
+++ app/models/child_emergency_contact.py
@@ -1,1 +1,18 @@
+class ChildEmergencyContact(db.Model):
+    __tablename__ = "child_emergency_contact"
+    __table_args__ = {"schema": "dbo"}
 
+    contact_id = db.Column(db.Integer, primary_key=True)
+
+    child_id = db.Column(
+        db.Integer,
+        db.ForeignKey("dbo.child.child_id", ondelete="CASCADE"),
+        index=True
+    )
+
+    first_name = db.Column(db.Unicode(100))
+    last_name = db.Column(db.Unicode(100))
+    relationship_to_child = db.Column(db.Unicode(100))
+    phone = db.Column(db.Unicode(50))
+
+    authorized_pickup = db.Column(db.Boolean, server_default=db.text("0"))
