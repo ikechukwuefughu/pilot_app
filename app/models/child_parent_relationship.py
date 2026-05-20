3a443e195c1cd8c1adebe6618b3fa05fcaeb5a94
ikechukwuefughu <75021390+ikechukwuefughu@users.noreply.github.com>
2026-05-20 22:01:28 +0100

    child_parent_relationship.py

--- app/models/child_parent_relationship.py
+++ app/models/child_parent_relationship.py
@@ -1,1 +1,23 @@
+class ChildParentRelationship(db.Model):
+    __tablename__ = "child_parent_relationship"
+    __table_args__ = {"schema": "dbo"}
 
+    relationship_id = db.Column(db.Integer, primary_key=True)
+
+    child_id = db.Column(
+        db.Integer,
+        db.ForeignKey("dbo.child.child_id", ondelete="CASCADE"),
+        index=True
+    )
+
+    parent_id = db.Column(
+        db.Integer,
+        db.ForeignKey("dbo.parent.parent_id", ondelete="CASCADE"),
+        index=True
+    )
+
+    relationship_type = db.Column(db.Unicode(100), nullable=False)
+
+    legal_guardian = db.Column(db.Boolean, server_default=db.text("0"))
+    authorized_pickup = db.Column(db.Boolean, server_default=db.text("1"))
+    emergency_contact = db.Column(db.Boolean, server_default=db.text("1"))
