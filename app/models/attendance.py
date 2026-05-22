349d3eb7d2ba0b7df2f0340e9ed07f8d08125158
ikechukwuefughu <75021390+ikechukwuefughu@users.noreply.github.com>
2026-05-22 18:33:10 +0100

    attendance.py

--- app/models/attendance.py
+++ app/models/attendance.py
@@ -1,12 +1,1 @@
-from app.config import get_db_connection
 
-def get_attendance_by_date(date):
-    conn = get_db_connection()
-
-    rows = conn.execute("""
-        SELECT * FROM child_attendance
-        WHERE attendance_date = ?
-    """, (date,)).fetchall()
-
-    conn.close()
-    return rows
